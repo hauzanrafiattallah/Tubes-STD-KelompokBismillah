d9548e0d83dc6e72d100d604a80d4df63e5fb681
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-11-18 20:42:16 +0700

    Test Upload File || Hauzan

---
+++ header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_INCLUDED
+#define HEADER_INCLUDED
+
+#include <iostream>
+using namespace std;
+
+
+void printHello();
+
+#endif // HEADER_INCLUDED
---
+++ source.cpp
@@ -0,0 +1,5 @@
+#include "header.h" 
+
+void printHello() {
+    cout << "Hello World" << endl;
+}
